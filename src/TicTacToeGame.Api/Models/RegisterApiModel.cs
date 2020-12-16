178dd39e2bca8fc8da4ab6b7c36a5b336bfb57b6
Rex Reyes III <rex.reyesiii@live.com.ph>
2020-12-16 17:08:59 +0800

    Exclamation Pt Fix

--- src/TicTacToeGame.Api/Models/RegisterApiModel.cs
+++ src/TicTacToeGame.Api/Models/RegisterApiModel.cs
@@ -10,9 +10,7 @@ namespace TicTacToeGame.Api.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [CustomPasswordFormat(ErrorMessage = "Passwords must be at least 6 characters and contains the following: upper case (A-Z), lower case (a-z), number (0-9) and a non alphanumeric special character (e.g. !@#$%^&*)")]
+        [CustomPasswordFormat(ErrorMessage = "Passwords must be at least 6 characters and max 100 that contains the following: upper case (A-Z), lower case (a-z), number (0-9) and a non alphanumeric special character (e.g. !@#$%^&*)")]
         public string Password { get; set; }
     }
 }
